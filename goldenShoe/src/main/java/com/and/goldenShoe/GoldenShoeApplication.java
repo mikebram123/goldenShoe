f67fc8a9444f104de4a90e1e5bb285a791c36560
Michael61296 <Michael61296@GB-LD1LPW10G003.mshome.net>
2021-01-19 14:40:45 +0000

    initial commit

---
+++ goldenShoe/src/main/java/com/and/goldenShoe/GoldenShoeApplication.java
@@ -0,0 +1,13 @@
+package com.and.goldenShoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenShoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenShoeApplication.class, args);
+	}
+
+}
---
+++ goldenShoe/src/test/java/com/and/goldenShoe/GoldenShoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.and.goldenShoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenShoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
